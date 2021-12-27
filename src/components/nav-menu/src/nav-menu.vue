043488088311cefe6742e3c794679c5e3f59c006
Mrxiaov <51041769+Mrxiaov@users.noreply.github.com>
2021-12-27 14:21:26 +0800

    feat(nav-menu): add nav-menu fold and expand

--- src/components/nav-menu/src/nav-menu.vue
+++ src/components/nav-menu/src/nav-menu.vue
@@ -2,11 +2,12 @@
   <div class="nav-menu">
     <div class="logo">
       <img class="img" src="@/assets/img/logo.svg" alt="" />
-      <span class="title">Vue3+TS</span>
+      <span v-if="!collapse" class="title">Vue3+TS</span>
     </div>
     <el-menu
       default-active="2"
       class="el-menu-vertical"
+      :collapse="collapse"
       background-color="#0c2135"
       text-color="#b7bdc3"
       active-text-color="#0a60bd"
@@ -57,6 +58,12 @@ import { computed, defineComponent } from 'vue'
 import { useStore } from '@/store'
 
 export default defineComponent({
+  props: {
+    collapse: {
+      type: Boolean,
+      default: false
+    }
+  },
   setup() {
     const store = useStore()
     const userMenus = computed(() => store.state.login.userMenus)
